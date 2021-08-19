6b39ff33ae300c5b924af4737011c0a91fcea9f2
Joao Pedro <joaopedroalb.dev@gmail.com>
2021-08-19 20:14:27 -0300

    Correcao logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ export default ({black})=>{
     return(
         <header className={black ?"black" : ''}>
             <div className="header--logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"/>
+                <img src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABbbXpFsC33lP-nPka2_0H17FDv2sQRtEEn0Ci_VMii8aJWfh2mOu_5JAgT7l22tPLjGqcRweDcAxPQKqodpzkBTzbu_38dxSc-jV.png?r=2d6"/>
             </div>
             <div className="header--user">
                 <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/>
